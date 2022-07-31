14f41fc5ae8eaf26eeb5a2dd5f33417edd57453f
Anjali Gupta <78307160+Anjali0906@users.noreply.github.com>
2022-07-31 14:44:48 +0530

    initial commit

---
+++ src/routes/_profile/bg.svelte
@@ -0,0 +1,53 @@
+<div class="bg">
+	<div class="block">
+		<div class="circle c1" />
+		<!-- <div class="circle c2" /> -->
+	</div>
+</div>
+
+<style>
+	.bg {
+		position: absolute;
+		inset: 0;
+		z-index: -2;
+
+		background-color: var(--color3);
+
+		/* width: min(100%, var(--ms01)); */
+		margin: auto;
+
+		pointer-events: none;
+	}
+
+	.bg .block {
+		position: relative;
+		background-color: rgba(255, 240, 240, 0.716);
+		/* display: flex;
+		flex-direction: column; */
+		height: 100%;
+		width: min(100%, var(--ms01));
+		margin: auto;
+		/* padding: var(--pad2);
+		padding-bottom: var(--mobileBottom); */
+		overflow: hidden;
+	}
+
+	.circle {
+		position: absolute;
+		background-color: rgba(195, 167, 43, 0.984);
+		width: var(--size);
+		height: var(--size);
+		border-radius: 50%;
+	}
+	.c1 {
+		--size: min(800px, 80vh);
+		top: -20%;
+		left: 30%;
+	}
+	/* .c2 {
+		--size: 5vw;
+
+		top: 100px;
+		left: 50px;
+	} */
+</style>
---
+++ src/routes/_profile/details.svelte
@@ -0,0 +1,33 @@
+<div id="profile_item_details">
+	<div id="profile_item_name">Item Name</div>
+	<div class="item">Rs <span id="profile_item_price">Item Price </span></div>
+	<div id="profile_item_desc" class="item">
+		
+		<br /> 
+	</div>
+</div>
+
+<style>
+	#profile_item_details {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		right: 0;
+
+		height: 22%;
+
+		padding: var(--pad);
+
+		pointer-events: auto;
+	}
+
+	@media screen and (min-width: 800px) {
+		#profile_item_details {
+			padding: var(--pad2);
+		}
+	}
+
+	.item {
+		font-size: 1.5rem;
+	}
+</style>
